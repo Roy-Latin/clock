f7b82e96cecfd8bb21386937307a61cd4510da65
Roy-Latin <roeilat@gmail.com>
2024-03-06 15:42:44 +0200

    first commit

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all origins
+
+@app.route('/')
+def get_current_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return f"The current time is: {current_time}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
