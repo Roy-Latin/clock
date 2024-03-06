43cc52d8ef34c9412f3bdf13b1e8ca4071666abd
Roy-Latin <roeilat@gmail.com>
2024-03-06 23:15:49 +0200

    test

--- get_public_ip/get_public_ip.php
+++ get_public_ip/get_public_ip.php
@@ -1,7 +1,7 @@
 <?php
 
 // Execute the Bash script to fetch the public IP address
-$public_ip = shell_exec('./get_public_ip.sh');
+$public_ip = shell_exec('/home/roy/app1/get_public_ip/get_public_ip.sh');
 
 // Output the public IP address
 echo $public_ip;
