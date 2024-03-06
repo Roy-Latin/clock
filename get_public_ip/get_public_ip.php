4845d24f601efc1819c3a5239d25b1611ffd75f4
Roy-Latin <roeilat@gmail.com>
2024-03-07 01:10:41 +0200

    removed

--- get_public_ip/get_public_ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Execute the Bash script to fetch the public IP address
-$public_ip = shell_exec('/home/roy/app1/get_public_ip/get_public_ip.sh');
-
-// Output the public IP address
-echo $public_ip;
-
-?>
--- get_public_ip/get_public_ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Fetch public IP address using an external service
-public_ip=$(curl -s https://api.ipify.org)
-
-# Print the public IP address
-echo $public_ip
